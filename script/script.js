45a6aa33261bb7c6655aa2c0b8b9ef22cc20dde5
Bakaeva <63263824+Bakaeva@users.noreply.github.com>
2020-08-18 16:01:51 +0300

    First commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let money = 1;
+let income = true;
+let addExpenses = true;
+let deposit = 1000;
+let mission = 'Миссия выполнима';
+let period = 1;
+alert('Сообщение с любым текстом');
+console.log('Сообщение с любым текстом');
