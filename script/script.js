88dd7a5a44874eae17638528df4637801955aba4
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2020-08-18 17:35:22 +0300

    Create branch lesson02

--- script/script.js
+++ script/script.js
@@ -1,8 +1,19 @@
-let money = 1;
-let income = true;
-let addExpenses = true;
-let deposit = 1000;
-let mission = 'Миссия выполнима';
-let period = 1;
-alert('Сообщение с любым текстом');
-console.log('Сообщение с любым текстом');
+let money = 40000;
+let income = 'фриланс';
+let addExpenses = 'квартплата, интернет, проезд, образование, питание, одежда';
+let deposit = true;
+let mission = 100000;
+let period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+let arr = addExpenses.toLowerCase().split(', ');
+console.log(arr);
+
+let budgetDay = money / 30;
+console.log(budgetDay);
