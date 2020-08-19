75a4be4b69ed294766166032da3e87839d57c8b1
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2020-08-19 15:57:26 +0300

    Create branch lesson03

--- script/script.js
+++ script/script.js
@@ -1,19 +1,48 @@
-let money = 40000;
+'use strict';
+
+let money = parseInt(prompt('Ваш месячный доход ?'));
+if (Number.isNaN(money))
+  money = 0;
+console.log('typeof money: ' + typeof money);
+
 let income = 'фриланс';
-let addExpenses = 'квартплата, интернет, проезд, образование, питание, одежда';
-let deposit = true;
-let mission = 100000;
-let period = 12;
+console.log('typeof income: ' + typeof income);
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log('typeof deposit: ' + typeof deposit);
 
-let arr = addExpenses.toLowerCase().split(', ');
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+if (addExpenses === null)
+  addExpenses = '';
+console.log('addExpenses.length: ' + addExpenses.length);
+let arr = addExpenses.toLowerCase().split(",");
 console.log(arr);
 
-let budgetDay = money / 30;
-console.log(budgetDay);
+let mission = 100000;
+console.log('Цель: заработать ' + mission + ' рублей');
+
+let expenses1 = prompt('Введите обязательную статью расходов:');
+let amount1 = parseInt(prompt('Во сколько это обойдется?'));
+if (Number.isNaN(amount1))
+  amount1 = 0;
+let expenses2 = prompt('Введите обязательную статью расходов:');
+let amount2 = parseInt(prompt('Во сколько это обойдется?'));
+if (Number.isNaN(amount2))
+  amount2 = 0;
+
+let budgetMonth = money - amount1 - amount2;
+console.log('Бюджет на месяц: ' + budgetMonth);
+
+let period = Math.ceil(mission / budgetMonth);
+console.log('Цель будет достигнута за ' + period + ' месяцев');
+
+let budgetDay = budgetMonth / 30;
+console.log('Бюджет на день: ' + budgetDay + ' ~ ' + Math.floor(budgetDay));
+
+console.log(budgetDay > 1200 ? 'У вас высокий уровень дохода' :
+  budgetDay === 1200 ? 'Ваш уровень дохода между высоким и средним' :
+  budgetDay > 600 ? 'У вас средний уровень дохода' :
+  budgetDay === 600 ? 'Ваш уровень дохода между средним и низким' :
+  budgetDay > 0 ? 'К сожалению, у вас уровень дохода ниже среднего' :
+  budgetDay === 0 ? 'Странно, что Вы ещё живы' :
+  'Что-то пошло не так');
